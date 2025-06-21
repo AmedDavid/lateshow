84bd8a6db6c0d517e6947c0dae6406709c4c1acd
David Amedi <amukoahdavid@gmail.com>
2025-06-21 11:13:59 +0300

    Add sys.path adjustment for running the app directly

--- server/app.py
+++ server/app.py
@@ -1,3 +1,10 @@
+import os
+import sys
+
+# Add the parent directory to sys.path if running directly
+if __name__ == '__main__':
+    sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from server import create_app
 from server.models import db, Episode, Guest, Appearance
 
